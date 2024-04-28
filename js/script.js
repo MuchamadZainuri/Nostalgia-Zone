1665e8942b4aa92c2e16118a56a7f7d6b371be9f
Muchamad Zainuri <muchamadzainuri6@gmail.com>
2024-04-28 22:59:41 +0700

    Mini Project E-commerce Landing Page

---
+++ js/script.js
@@ -0,0 +1,76 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => { 
+    searchForm.classList.toggle('active');
+}
+
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    if(window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+window.onload = () => {
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+    fadeOut();
+}
+
+function loader() {
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut() { 
+    setTimeout(loader, 3500);
+}
+
+var swiper = new Swiper(".toys-slider", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 1,
+        },
+    },
+});
+
+var swiper = new Swiper(".rare-slider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
